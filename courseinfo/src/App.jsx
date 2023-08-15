d8683ca6f84698a358f66b6bcbac2cfeb393ed00
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-15 18:38:27 +0300

    Add courseinfo folder

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,32 @@
+const App = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header />
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <h1>Half Stack application development</h1>
+  )
+}
+
+export default App;
