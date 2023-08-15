baed4b3ac156a89b6735849b8bd75b3e8c0e5e5b
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-15 18:56:34 +0300

    Refactor the code to consist of three new components

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const App = () => {
+  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -8,24 +9,34 @@ const App = () => {
 
   return (
     <div>
-      <Header />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <h1>Half Stack application development</h1>
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.total}
+    </p>
   )
 }
 
