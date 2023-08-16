188a02c386d1f5d9aa2512a6294d3da4da0c587d
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-16 20:45:54 +0300

    Refactor Content component and add Part component

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -6,13 +6,18 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const parts = [
+    { part: part1, exercises: 10},
+    { part: part2, exercises: 7 },
+    { part: part3, exercises: 14 },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content parts={parts} />
+      {/* <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} /> */}
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
@@ -26,6 +31,14 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    props.parts.map((el) => (
+      <Part part={el.part} exercise={el.exercises} />
+    ))
+  )
+}
+
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercise}
     </p>
@@ -34,6 +47,7 @@ const Content = (props) => {
 
 const Total = (props) => {
   return (
+
     <p>
       Number of exercises {props.total}
     </p>
