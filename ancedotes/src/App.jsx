f1ad9df08ce76cc0f945e4951aafa6f7838df653
Ruiyot Abby <ruiyotabby@gmail.com>
2023-08-18 21:07:57 +0300

    Modify App component

--- ancedotes/src/App.jsx
+++ ancedotes/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function App() {
-  const anecdotes = [
+  const ancedotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
@@ -14,13 +14,54 @@ function App() {
     'Program testing can be used to show the presence of bugs, but never to show their absence!'
   ]
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(10).fill(0,0,10));
+
+  const nextAncedote = () => {
+    let random = Math.round(Math.random()*10);
+    if(random > 9) {
+      random = Math.round(Math.random()*10);
+    }
+    setSelected(random);
+  }
+
+  const addVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  }
+
+  const sortAncedotes = () => {
+    const copy = [...votes];
+    copy.sort().reverse()
+    const index = votes.indexOf(copy[0])
+    return (index)
+  }
 
   return (
     <div className="App">
-      <p>{anecdotes[selected]}</p>
+      <Header text='Ancedote of the day' />
+      <p>{ancedotes[selected]}</p>
+      <Button handleClick={addVote} text='vote' />
+      <Button handleClick={nextAncedote} text='next ancedote' />
+      <Header text='Ancedote with most votes' />
+      <Favourite count={sortAncedotes()} text={ancedotes[sortAncedotes()]} />
     </div>
   );
+}
+
+const Header = ({text}) => (
+  <h2>{text}</h2>
+)
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Favourite = ({count,text}) => {
+  return (
+    <p>{text}</p>
+  )
 }
 
 export default App;
